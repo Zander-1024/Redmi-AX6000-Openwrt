f32f2dfa6ae504b15201303390cfc48536545f99
Nashan <34827878+zhuang1234@users.noreply.github.com>
2025-05-05 21:39:42 +0800

    优化 diy-part1.sh 脚本，调整源添加顺序并清理不必要的包

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,22 +16,19 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
-sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-./scripts/feeds update -a
-./scripts/feeds install -a -f -p kenzo
-./scripts/feeds install -a -f -p small
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+# ./scripts/feeds install -a 
 
 
-./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata
-./scripts/feeds install -f -p mosdns luci-app-mosdns
-find ./ -name v2ray-geodata | xargs rm -rf
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-svn export https://github.com/immortalwrt/luci/branches/openwrt-24.10/applications/luci-app-vlmcsd package/luci-app-vlmcsd
-svn export https://github.com/immortalwrt/packages/branches/openwrt-24.10/net/vlmcsd  package/vlmcsd
+# svn export https://github.com/immortalwrt/luci/branches/openwrt-24.10/applications/luci-app-vlmcsd package/luci-app-vlmcsd
+# svn export https://github.com/immortalwrt/packages/branches/openwrt-24.10/net/vlmcsd  package/vlmcsd
+
 sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
 ./scripts/feeds update -i && ./scripts/feeds install -a
