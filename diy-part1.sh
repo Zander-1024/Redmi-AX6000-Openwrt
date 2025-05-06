47e9be1d9b4b54eb1e4aebb20e48367f49bbe00b
Zander <34827878+Zander-1024@users.noreply.github.com>
2025-05-06 09:38:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,sm
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-# ./scripts/feeds install -a 
+./scripts/feeds install -a 
 
 
 # svn export https://github.com/immortalwrt/luci/branches/openwrt-24.10/applications/luci-app-vlmcsd package/luci-app-vlmcsd
